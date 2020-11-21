cb62feddd643a146180e93ec6c2bac07e4103cf9
Bruno Cabral <55759357+CABRALBRUNO000@users.noreply.github.com>
2020-11-21 20:32:40 -0300

    primeiro commit

--- apps/voluntary/src/app/app.component.spec.ts
+++ apps/voluntary/src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
-  });
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -24,8 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to voluntary!'
-    );
+    expect(compiled.querySelector('.content span').textContent).toContain('voluntary app is running!');
   });
 });
