d5fda2d4d14364dadd890c75a33dea8dec60d4f8
Bruno Cabral <55759357+CABRALBRUNO000@users.noreply.github.com>
2020-11-21 18:51:42 -0300

    initial commit

---
+++ apps/voluntary/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aid-up-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'voluntary';
+}
